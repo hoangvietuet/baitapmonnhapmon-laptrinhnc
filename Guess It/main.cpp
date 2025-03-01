4df13b3f087de0e9db4d310c29ef6d25ce1c3127
Nguyễn Hoàng Việt <ln0367338@gmail.com>
2025-03-01 15:07:02 +0700

    Guess it

--- Guess It/main.cpp
+++ Guess It/main.cpp
@@ -10,18 +10,38 @@ int n;
 cin >> n;
 return n;
 }
-void printAnswer(int guess, int secretNumber){
+void printAnswer(int guess, int secretNumber, int score){
 if (guess > secretNumber) cout << "your number is big too\n";
 else if (guess < secretNumber) cout << "your number is small too\n";
-else cout << "correct";
+else{
+     if (score > 100){
+        cout << "Game Over!";
+     }
+     else{
+        cout << "Correct!" << endl;
+        cout << "your score is: " << 100 - score << endl;
+     }
+}
 }
 int main()
 {
-int secretNumber = generateRandomNumber();
-int guess;
-do {
-guess = getPlayerGuess();
-printAnswer(guess, secretNumber);
-} while (guess != secretNumber);
-return 0;
+    char kitu ;  // xem ng choi muon choi nua khong
+    int highscore = 0; // diem so cao nhat
+do{
+    int score = 1; // diem cua van choi
+    cout << "Please enter a number from 1 to 100:\n";
+    int secretNumber = generateRandomNumber();
+    int guess;
+   do {
+    guess = getPlayerGuess();
+    printAnswer(guess, secretNumber, score);
+    score++;
+      }while (guess != secretNumber);
+     if (100 - score > highscore) highscore = 100 - score;
+    cout << "do you want to play again ";
+    cout << "If yes please enter 'y':";
+     cin >> kitu;
+}while(kitu == 'y');
+  cout << "Your highscore : " << highscore << endl;;
+  cout << "Thanks";
 }
